5dac364144eae01d8aad11d19de9d7386a64e1f4
Danilo Castro <danilo.castro@grupoarliz.pt>
2025-10-27 19:17:02 +0000

    Melhoramento icons - instalação depedências

--- app/src/main/java/ipca/example/calculator/CalculatorView.kt
+++ app/src/main/java/ipca/example/calculator/CalculatorView.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Backspace
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -99,7 +97,6 @@ fun CalculatorView(
             CalculatorButton(label = "%", isOperation = true, onClick = onOperationClick)
             CalculatorButton(
                 label = "C",
-                icon = Icons.AutoMirrored.Filled.Backspace,
                 isOperation = true, // To keep the orange color
                 onClick = onControlClick)
         }
